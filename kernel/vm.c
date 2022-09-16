738a97e5f30192cf1eab0e0a53437b7b91ebbebf
SunnyLimc <limc@limcode.cn>
2022-09-16 11:56:22 +0000

    lab: finish "print a page table"

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void _vmprint(pagetable_t ptepm, int level) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = ptepm[i];
+    if (pte & PTE_V) {
+      pagetable_t pa = (pagetable_t)PTE2PA(pte);
+      for (int i = 2; i > level; i--) {
+        printf(".. ");
+      }
+      printf("..%d: ", i);
+      printf("pte: %p pa: %p\n", pte, pa);
+      if (level > 0) {
+        _vmprint(pa, level - 1);
+      }
+    }
+  }
+}
+
+int vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  _vmprint(pagetable, 2);
+  return 0;
+}
